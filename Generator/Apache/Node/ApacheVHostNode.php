cb22c654f13832e23b9144b77865c88e0bf52a97
Jakub Turek <ja@kubaturek.pl>
2015-05-30 23:33:53 +0200

    Add setting address of Apahe Vhost

--- Generator/Apache/Node/ApacheVHostNode.php
+++ Generator/Apache/Node/ApacheVHostNode.php
@@ -2,6 +2,7 @@
 
 namespace Eps\VhostGeneratorBundle\Generator\Apache\Node;
 
+use Eps\VhostGeneratorBundle\Generator\Exception\ValidationException;
 use Eps\VhostGeneratorBundle\Generator\Node\NodeInterface;
 
 /**
@@ -11,6 +12,30 @@ use Eps\VhostGeneratorBundle\Generator\Node\NodeInterface;
  */
 class ApacheVHostNode implements NodeInterface
 {
+    /**
+     * Address and port for Apache VHost in format: [ADDRESS]:[PORT]
+     */
+    const ADDRESS = 'address';
+
+    /**
+     * Regexp for valid IP address or asterisk (*)
+     */
+    const IP_REGEXP = '/^((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|\*)$/';
+
+    /**
+     * Regexp for valid port number (0..65535)
+     */
+    const PORT_NUMBER_REGEXP = '/^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/';
+
+    /**
+     * @var array Attributes of the VHost
+     */
+    protected $attributes = [
+        self::ADDRESS => '*:80'
+    ];
+
+    protected $properties = [];
+
     /**
      * {@inheritdoc}
      */
@@ -24,7 +49,7 @@ class ApacheVHostNode implements NodeInterface
      */
     public function getAttributes()
     {
-        return [];
+        return $this->attributes;
     }
 
     /**
@@ -33,5 +58,25 @@ class ApacheVHostNode implements NodeInterface
     public function getConfiguration()
     {
         return [];
+    }
+
+    /**
+     * Sets the IP Address of the Apache VHost
+     *
+     * @param string $ipAddress IP Address
+     * @param integer|string $port Port number
+     *
+     * @return self
+     * @throws ValidationException
+     */
+    public function setAddress($ipAddress, $port = 80)
+    {
+        if (!preg_match(self::IP_REGEXP, $ipAddress) || !preg_match(self::PORT_NUMBER_REGEXP, $port)) {
+            throw new ValidationException('Invalid IP address or port number');
+        }
+
+        $this->attributes[self::ADDRESS] = $ipAddress . ':' . $port;
+
+        return $this;
     }
 }
--- Tests/Generator/Apache/Node/ApacheVHostNodeTest.php
+++ Tests/Generator/Apache/Node/ApacheVHostNodeTest.php
@@ -16,9 +16,73 @@ class ApacheVHostNodeTest extends \PHPUnit_Framework_TestCase
      */
     public function itShouldReturnItsName()
     {
-        $expectedName = 'VirtualHost';
         $vhostNode = new ApacheVHostNode();
+        $expectedName = 'VirtualHost';
         $actual = $vhostNode->getName();
         $this->assertEquals($expectedName, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldHaveWildcardAddressAttributeByDefault()
+    {
+        $vhostNode = new ApacheVHostNode();
+        $expectedAttributes = [
+            ApacheVHostNode::ADDRESS => '*:80'
+        ];
+        $actual = $vhostNode->getAttributes();
+        $this->assertEquals($expectedAttributes, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldBeAbleToSetIpAddress()
+    {
+        $vhostNode = new ApacheVHostNode();
+
+        $vhostNode->setAddress('127.0.0.1');
+        $expected = [
+            ApacheVHostNode::ADDRESS => '127.0.0.1:80'
+        ];
+        $actual = $vhostNode->getAttributes();
+        $this->assertEquals($expected, $actual);
+
+        $vhostNode->setAddress('*', 443);
+        $expected = [
+            ApacheVHostNode::ADDRESS => '*:443'
+        ];
+        $actual = $vhostNode->getAttributes();
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function invalidAddressesProvider()
+    {
+        return [
+            'wrong_ip' => [
+                'ip_address' => '127.0.0.0.0.0.1',
+                'port' => '80'
+            ],
+            'wrong port' => [
+                'ip_addres' => '*',
+                'port' => 'a80ba'
+            ],
+            'port_exceeded_limit_of_65535' => [
+                'ip_address' => '*',
+                'port' => '90843028028'
+            ]
+        ];
+    }
+
+    /**
+     * @test
+     * @expectedException \Eps\VHostGeneratorBundle\Generator\Exception\ValidationException
+     * @dataProvider invalidAddressesProvider
+     */
+    public function itShouldNotAllowToSetInvalidAddress($ipAddress, $port)
+    {
+        $vhostNode = new ApacheVHostNode();
+        $vhostNode->setAddress($ipAddress, $port);
     }
 }
