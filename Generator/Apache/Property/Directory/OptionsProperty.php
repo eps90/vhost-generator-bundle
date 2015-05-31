e2842472f8f4ac747178a9994e2bb7608c84628b
Jakub Turek <ja@kubaturek.pl>
2015-05-31 15:43:46 +0200

    Create OptionsProperty

---
+++ Generator/Apache/Property/Directory/OptionsProperty.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Eps\VhostGeneratorBundle\Generator\Apache\Property\Directory;
+
+use Eps\VhostGeneratorBundle\Generator\Property\ValidatablePropertyInterface;
+
+/**
+ * Class OptionsProperty
+ * @package Eps\VhostGeneratorBundle\Generator\Apache\Property\Directory
+ * @author Jakub Turek <ja@kubaturek.pl>
+ */
+class OptionsProperty implements ValidatablePropertyInterface
+{
+    const NAME = 'Options';
+
+    const ALL = 'All';
+
+    const NONE = 'None';
+
+    const EXEC_GGI = 'ExecCGI';
+
+    const FOLLOW_SYM_LINKS = 'FollowSymLinks';
+
+    const INCLUDES = 'Includes';
+
+    const INCLUDES_NO_EXEC = 'IncludesNOEXEC';
+
+    const INDEXES = 'Indexes';
+
+    const MULTI_VIEWS = 'MultiViews';
+
+    const SYM_LINKS_IF_OWNER_MATCH = 'SymLinksIfOwnerMatch';
+
+    /**
+     * @var array Options
+     */
+    protected $options = [];
+
+    /**
+     * Constructor.
+     * Sets the options. Input array should accept option name as key and boolean as value.
+     * Value in array determines whether the option will be enabled or not.
+     *
+     * @param array $options
+     */
+    public function __construct(array $options)
+    {
+        $result = [];
+        $includesAll = in_array(self::ALL, $options, true) || in_array(self::ALL, array_keys($options), true);
+        $includesNone = in_array(self::NONE, $options, true) || in_array(self::NONE, array_keys($options), true);
+
+        foreach ($options as $optionKey => $optionValue) {
+            $key = $optionKey;
+            $value = $optionValue;
+            if (!is_string($optionKey)) {
+                $key = $optionValue;
+                $value = true;
+            }
+
+            if ($key == self::ALL || $key == self::NONE) {
+                if ($includesAll && $includesNone) {
+                    $result[self::ALL] = true;
+                } else {
+                    $result[$key] = $value;
+                }
+            } elseif ($key == self::MULTI_VIEWS && $includesAll) {
+                $result[$key] = $value;
+            } elseif (!$includesAll && !$includesNone) {
+                $result[$key] = $value;
+            }
+        }
+
+        $this->options = $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return self::NAME;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getValue()
+    {
+        $result = [];
+
+        if (array_fill(0, count($this->options), true) == array_values($this->options)) {
+            $result = array_keys($this->options);
+        } else {
+            foreach ($this->options as $optionName => $enabled) {
+                $result[] = ($enabled ? '+' : '-') . $optionName;
+            }
+        }
+
+        return implode(' ', $result);
+    }
+
+    /**
+     * Returns array of values
+     *
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isValid()
+    {
+        $acceptableValues = [
+            OptionsProperty::ALL,
+            OptionsProperty::NONE,
+            OptionsProperty::EXEC_GGI,
+            OptionsProperty::FOLLOW_SYM_LINKS,
+            OptionsProperty::INCLUDES,
+            OptionsProperty::INCLUDES_NO_EXEC,
+            OptionsProperty::INDEXES,
+            OptionsProperty::MULTI_VIEWS,
+            OptionsProperty::SYM_LINKS_IF_OWNER_MATCH
+        ];
+
+        foreach ($this->options as $optionName => $optionValue) {
+            if (!in_array($optionName, $acceptableValues)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Tests/Generator/Apache/Property/Directory/OptionsPropertyTest.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace Eps\VhostGeneratorBundle\Tests\Generator\Apache\Property\Directory;
+
+use Eps\VhostGeneratorBundle\Generator\Apache\Property\Directory\OptionsProperty;
+
+/**
+ * Class OptionsPropertyTest
+ * @package Eps\VhostGeneratorBundle\Tests\Generator\Apache\Property\Directory
+ * @author Jakub Turek <ja@kubaturek.pl>
+ */
+class OptionsPropertyTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldHaveValidName()
+    {
+        $property = new OptionsProperty([OptionsProperty::ALL]);
+        $actual = $property->getName();
+        $expected = 'Options';
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function validValuesProvider()
+    {
+        return [
+            [OptionsProperty::ALL],
+            [OptionsProperty::NONE],
+            [OptionsProperty::EXEC_GGI],
+            [OptionsProperty::FOLLOW_SYM_LINKS],
+            [OptionsProperty::INCLUDES],
+            [OptionsProperty::INCLUDES_NO_EXEC],
+            [OptionsProperty::INDEXES],
+            [OptionsProperty::MULTI_VIEWS],
+            [OptionsProperty::SYM_LINKS_IF_OWNER_MATCH]
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider validValuesProvider
+     */
+    public function itShouldBeAbleToSetOptionValue($optionValue)
+    {
+        $property = new OptionsProperty([$optionValue => true]);
+        $actual = $property->getOptions();
+        $expected = [
+            $optionValue => true
+        ];
+        $this->assertEquals($expected, $actual);
+        $this->assertTrue($property->isValid());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldDetermineWhetherOptionsAreValid()
+    {
+        $invalidPropertyName = 'SomeNonExistingProperty';
+        $property = new OptionsProperty([$invalidPropertyName => true]);
+        $this->assertFalse($property->isValid());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldAcceptSimpleArrays()
+    {
+        $property = new OptionsProperty([OptionsProperty::EXEC_GGI]);
+        $expected = [
+            OptionsProperty::EXEC_GGI => true
+        ];
+        $actual = $property->getOptions();
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldFilterOutAllValuesButMultiViewsIfOptionsContainAll()
+    {
+        $options = [
+            OptionsProperty::INCLUDES => true,
+            OptionsProperty::INDEXES => false,
+            OptionsProperty::ALL => true,
+            OptionsProperty::MULTI_VIEWS => true
+        ];
+        $property = new OptionsProperty($options);
+        $expected = [
+            OptionsProperty::ALL => true,
+            OptionsProperty::MULTI_VIEWS => true
+        ];
+        $actual = $property->getOptions();
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldFilterOutAllValuesIfOptionsContainNone()
+    {
+        $options = [
+            OptionsProperty::INCLUDES => true,
+            OptionsProperty::INDEXES => false,
+            OptionsProperty::NONE => true
+        ];
+        $property = new OptionsProperty($options);
+        $expected = [
+            OptionsProperty::NONE => true
+        ];
+        $actual = $property->getOptions();
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldContainAllIfAllAndNoneInOptions()
+    {
+        $options = [
+            OptionsProperty::ALL,
+            OptionsProperty::NONE
+        ];
+        $expected = [
+            OptionsProperty::ALL => true
+        ];
+
+        $property = new OptionsProperty($options);
+        $acutal = $property->getOptions();
+        $this->assertEquals($expected, $acutal);
+    }
+
+    public function optionsStringProvider()
+    {
+        return [
+            [
+                'options' => [
+                    OptionsProperty::EXEC_GGI => true,
+                    OptionsProperty::FOLLOW_SYM_LINKS => true,
+                ],
+                'optionsString' => 'ExecCGI FollowSymLinks'
+            ],
+            [
+                'options' => [
+                    OptionsProperty::INDEXES => true,
+                    OptionsProperty::INCLUDES => false
+                ],
+                'optionsString' => '+Indexes -Includes'
+            ],
+            [
+                'options' => [
+                    OptionsProperty::ALL
+                ],
+                'optionsString' => 'All'
+            ],
+            [
+                'options' => [
+                    OptionsProperty::NONE
+                ],
+                'optionsString' => 'None'
+            ],
+            [
+                'options' => [
+                    OptionsProperty::ALL,
+                    OptionsProperty::NONE
+                ],
+                'optionsString' => 'All'
+            ],
+            [
+                'options' => [
+                    OptionsProperty::ALL => true,
+                    OptionsProperty::MULTI_VIEWS => true
+                ],
+                'optionsString' => 'All MultiViews'
+            ],
+            [
+                'options' => [
+                    OptionsProperty::INDEXES => false,
+                    OptionsProperty::INCLUDES_NO_EXEC => false
+                ],
+                'optionsString' => '-Indexes -IncludesNOEXEC'
+            ]
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider optionsStringProvider
+     */
+    public function itShouldRenderOptionsString($options, $expected)
+    {
+        $property = new OptionsProperty($options);
+        $actual = $property->getValue();
+        $this->assertEquals($expected, $actual);
+    }
+}
