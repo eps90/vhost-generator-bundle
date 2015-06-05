bfa548d690496be81efa3771bfad350e936cca27
Jakub Turek <ja@kubaturek.pl>
2015-06-05 13:33:00 +0200

    Let AllowProperty extend StringProperty

--- Generator/Apache/Property/Directory/AllowProperty.php
+++ Generator/Apache/Property/Directory/AllowProperty.php
@@ -3,24 +3,16 @@
 namespace Eps\VhostGeneratorBundle\Generator\Apache\Property\Directory;
 
 use Eps\VhostGeneratorBundle\Generator\Property\PropertyInterface;
+use Eps\VhostGeneratorBundle\Generator\Property\StringProperty;
 
 /**
  * Class AllowProperty
  * @package Eps\VhostGeneratorBundle\Generator\Apache\Property\Directory
  * @author Jakub Turek <ja@kubaturek.pl>
- * @todo Let AllowProperty implement ValidatabePropertyInterface
- * @todo Allow complex expressions
  */
-class AllowProperty implements PropertyInterface
+class AllowProperty extends StringProperty
 {
     const NAME = 'Allow';
-
-    protected $value;
-
-    public function __construct($value)
-    {
-        $this->value = $value;
-    }
 
     /**
      * {@inheritdoc}
