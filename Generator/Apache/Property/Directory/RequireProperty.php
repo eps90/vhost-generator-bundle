6f7144978c408f326e70dc3f3b0de1dc7de60efd
Jakub Turek <ja@kubaturek.pl>
2015-06-05 13:33:19 +0200

    Remove todo from RequireProperty

--- Generator/Apache/Property/Directory/RequireProperty.php
+++ Generator/Apache/Property/Directory/RequireProperty.php
@@ -8,7 +8,6 @@ use Eps\VhostGeneratorBundle\Generator\Property\StringProperty;
  * Class RequireProperty
  * @package Eps\VhostGeneratorBundle\Generator\Apache\Property\Directory
  * @author Jakub Turek <ja@kubaturek.pl>
- * @todo Extend RequireProperty with advanced values such as ip, users, etc.
  */
 class RequireProperty extends StringProperty
 {
