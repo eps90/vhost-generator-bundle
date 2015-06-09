ff7d1746fdeb2da0223e71d250d03fd8508d4324
Jakub Turek <ja@kubaturek.pl>
2015-06-09 20:22:04 +0200

    Configure verbose messaging

--- Command/GenerateVHostCommand.php
+++ Command/GenerateVHostCommand.php
@@ -79,13 +79,28 @@ class GenerateVHostCommand extends ContainerAwareCommand
         $vhostsPath = $this->getContainer()->getParameter('vhost_generator.apache.vhosts_path');
         $outputFile = $this->getContainer()->getParameter('vhost_generator.apache.output_file');
 
+        $output->write("<info>Generating vhost config contents...</info>");
         $configsContent = $this->generator->generate($vhosts);
+        $output->writeln('<info>OK</info>');
+
         $filePath = $this->getTempFileName($configsContent);
 
         $this->fileSystem->dumpFile($filePath, $configsContent);
-        $this->executeShellCommand("sudo cp $filePath $vhostsPath/$outputFile", $output);
-        $this->executeShellCommand("sudo a2ensite $outputFile", $output);
-        $this->executeShellCommand("sudo service apache2 reload", $output);
+        $this->executeShellCommand(
+            "sudo cp $filePath $vhostsPath/$outputFile",
+            "Copying temporary config to vhosts path...",
+            $output
+        );
+        $this->executeShellCommand(
+            "sudo a2ensite $outputFile",
+            "Enabling new VHost config...",
+            $output
+        );
+        $this->executeShellCommand(
+            "sudo service apache2 reload",
+            "Restating services...",
+            $output
+        );
     }
 
     /**
@@ -105,9 +120,15 @@ class GenerateVHostCommand extends ContainerAwareCommand
      * @param $command
      * @param OutputInterface $output
      */
-    private function executeShellCommand($command, OutputInterface $output)
+    private function executeShellCommand($command, $comment, OutputInterface $output)
     {
-        $output->write("<comment>Executing '$command'... </comment>");
+        $output->write("<info>$comment</info>");
+
+        if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
+            $output->writeln('');
+            $output->write("<comment>Executing '$command'... </comment>");
+        }
+
         $command = $this->processFactory->getProcess($command);
         $command->run();
         $output->writeln('<info>OK</info>');
