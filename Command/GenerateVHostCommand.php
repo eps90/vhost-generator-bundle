4aff92082e3fc5fe4c6c83bf384ea6e513de243a
Jakub Turek <ja@kubaturek.pl>
2015-06-17 20:58:33 +0200

    Let GenerateVHostCommand use InstallerInterface for installing configurations

--- Command/GenerateVHostCommand.php
+++ Command/GenerateVHostCommand.php
@@ -3,6 +3,7 @@
 namespace Eps\VhostGeneratorBundle\Command;
 
 use Eps\VhostGeneratorBundle\Generator\GeneratorInterface;
+use Eps\VhostGeneratorBundle\Installer\InstallerInterface;
 use Eps\VhostGeneratorBundle\Util\SymfonyProcessFactory;
 use Sensio\Bundle\GeneratorBundle\Command\Helper\QuestionHelper;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
@@ -20,33 +21,13 @@ use Symfony\Component\Filesystem\Filesystem;
 class GenerateVHostCommand extends ContainerAwareCommand
 {
     /**
-     * @var GeneratorInterface
+     * @var InstallerInterface
      */
-    private $generator;
+    private $installer;
 
-    /**
-     * @var Filesystem
-     */
-    private $fileSystem;
-
-    /**
-     * @var SymfonyProcessFactory
-     */
-    private $processFactory;
-
-    public function setGenerator(GeneratorInterface $generator)
+    public function setInstaller(InstallerInterface $installer)
     {
-        $this->generator = $generator;
-    }
-
-    public function setFileSystem(Filesystem $fileSystem)
-    {
-        $this->fileSystem = $fileSystem;
-    }
-
-    public function setProcessFactory(SymfonyProcessFactory $processFactory)
-    {
-        $this->processFactory = $processFactory;
+        $this->installer = $installer;
     }
 
     protected function configure()
@@ -74,79 +55,16 @@ class GenerateVHostCommand extends ContainerAwareCommand
             return;
         }
 
-        $vhosts = $this->getContainer()->getParameter('vhost_generator.apache.vhosts');
-        $vhostsPath = $this->getContainer()->getParameter('vhost_generator.apache.vhosts_path');
-        $outputFile = $this->getContainer()->getParameter('vhost_generator.apache.output_file');
+        $this->installer->install();
 
-        $output->write("<info>Generating vhost config contents...</info>");
-        $configsContent = $this->generator->generate($vhosts);
+//        $output->write("<info>Generating vhost config contents...</info>");
 
-        if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERY_VERBOSE) {
-            $output->writeln('');
-            $output->writeln('<comment>Following config is going to be applied: </comment>');
-            $output->writeln($configsContent);
-        }
+//        if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERY_VERBOSE) {
+//            $output->writeln('');
+//            $output->writeln('<comment>Following config is going to be applied: </comment>');
+//            $output->writeln($configsContent);
+//        }
 
-        $output->writeln('<info>OK</info>');
-
-        $filePath = $this->getTempFileName($configsContent);
-
-        $this->fileSystem->dumpFile($filePath, $configsContent);
-        $this->executeShellCommand(
-            "sudo cp $filePath $vhostsPath/$outputFile",
-            "Copying temporary config to vhosts path...",
-            $output
-        );
-        $this->executeShellCommand(
-            "sudo a2ensite $outputFile",
-            "Enabling new VHost config...",
-            $output
-        );
-        $this->executeShellCommand(
-            "sudo service apache2 reload",
-            "Restating services...",
-            $output
-        );
-    }
-
-    /**
-     * Returns temporary file name for apache vhost
-     *
-     * @param $configsContent
-     * @return string
-     */
-    private function getTempFileName($configsContent)
-    {
-        return sys_get_temp_dir() . '/vhost_' . substr(md5($configsContent), 0, 7);
-    }
-
-    /**
-     * Executes the command and outputs the status
-     *
-     * @param $command
-     * @param OutputInterface $output
-     */
-    private function executeShellCommand($command, $comment, OutputInterface $output)
-    {
-        $output->write("<info>$comment</info>");
-
-        if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
-            $output->writeln('');
-            $output->write("<comment>Executing '$command'... </comment>");
-        }
-
-        $command = $this->processFactory->getProcess($command);
-        $command->run();
-
-        if (!$command->isSuccessful()) {
-            throw new \RuntimeException($command->getErrorOutput());
-        }
-
-        if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERY_VERBOSE) {
-            $output->writeln('');
-            $output->write($command->getOutput());
-        }
-
-        $output->writeln('<info>OK</info>');
+//        $output->writeln('<info>OK</info>');
     }
 }
--- Tests/Command/GenerateVHostCommandTest.php
+++ Tests/Command/GenerateVHostCommandTest.php
@@ -34,123 +34,15 @@ class GenerateVHostCommandTest extends \PHPUnit_Framework_TestCase
      */
     public function itShouldGenerateApacheConfig()
     {
-        $sampleConfig = [
-            'prod' => [
-                'server_name' => 'example.com'
-            ]
-        ];
-        $vhostsPath = '/etc/apache2/sites-available';
-        $generatedConfig = 'some config';
-        $tmpFilePath = '/tmp/vhost_a24b250';
-        $outputFile = 'prod.conf';
-
-        $container = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
-
-        $containerParams = [
-            [
-                'name' => 'vhost_generator.apache.vhosts',
-                'value' => $sampleConfig
-            ],
-            [
-                'name' => 'vhost_generator.apache.vhosts_path',
-                'value' => $vhostsPath,
-            ],
-            [
-                'name' => 'vhost_generator.apache.output_file',
-                'value' => $outputFile
-            ]
-        ];
-
-        $this->mockContainerParameters($container, $containerParams);
-
-        $generator = $this->getMock('Eps\VhostGeneratorBundle\Generator\GeneratorInterface');
-        $generator->expects($this->once())
-            ->method('generate')
-            ->with($sampleConfig)
-            ->willReturn($generatedConfig);
-
-        $fileSystem = $this->getMock('Symfony\Component\Filesystem\Filesystem');
-        $fileSystem->expects($this->once())
-            ->method('dumpFile')
-            ->with($tmpFilePath, $generatedConfig);
-
-        $processFactory = $this->getMock('Eps\VhostGeneratorBundle\Util\SymfonyProcessFactory');
-        $this->expectProcess($processFactory, "sudo cp $tmpFilePath {$vhostsPath}/$outputFile", true);
-        $this->expectProcess($processFactory, "sudo a2ensite $outputFile", true);
-        $this->expectProcess($processFactory, 'sudo service apache2 reload', true);
+        $installer = $this->getMock('Eps\VhostGeneratorBundle\Installer\InstallerInterface');
+        $installer->expects($this->once())
+            ->method('install');
 
         $helpers = $this->getHelpers();
 
         $command = new GenerateVHostCommand();
-        $command->setContainer($container);
-        $command->setGenerator($generator);
-        $command->setFileSystem($fileSystem);
-        $command->setProcessFactory($processFactory);
         $command->setHelperSet(new HelperSet($helpers));
-
-        $input = $this->getMock('Symfony\Component\Console\Input\InputInterface');
-        $output = $this->getMock('Symfony\Component\Console\Output\OutputInterface');
-
-        $command->run($input, $output);
-    }
-
-    /**
-     * @test
-     * @expectedException \RuntimeException
-     */
-    public function itShouldThrowIfOneOfCommandHasFailed()
-    {
-        $sampleConfig = [
-            'prod' => [
-                'server_name' => 'example.com'
-            ]
-        ];
-        $vhostsPath = '/etc/apache2/sites-available';
-        $generatedConfig = 'some config';
-        $tmpFilePath = '/tmp/vhost_a24b250';
-        $outputFile = 'prod.conf';
-
-
-        $container = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
-        $containerParams = [
-            [
-                'name' => 'vhost_generator.apache.vhosts',
-                'value' => $sampleConfig
-            ],
-            [
-                'name' => 'vhost_generator.apache.vhosts_path',
-                'value' => $vhostsPath,
-            ],
-            [
-                'name' => 'vhost_generator.apache.output_file',
-                'value' => $outputFile
-            ]
-        ];
-
-        $this->mockContainerParameters($container, $containerParams);
-
-        $generator = $this->getMock('Eps\VhostGeneratorBundle\Generator\GeneratorInterface');
-        $generator->expects($this->once())
-            ->method('generate')
-            ->with($sampleConfig)
-            ->willReturn($generatedConfig);
-
-        $fileSystem = $this->getMock('Symfony\Component\Filesystem\Filesystem');
-        $fileSystem->expects($this->once())
-            ->method('dumpFile')
-            ->with($tmpFilePath, $generatedConfig);
-
-        $processFactory = $this->getMock('Eps\VhostGeneratorBundle\Util\SymfonyProcessFactory');
-        $this->expectProcess($processFactory, "sudo cp $tmpFilePath {$vhostsPath}/$outputFile", false);
-
-        $helpers = $this->getHelpers();
-
-        $command = new GenerateVHostCommand();
-        $command->setContainer($container);
-        $command->setGenerator($generator);
-        $command->setFileSystem($fileSystem);
-        $command->setProcessFactory($processFactory);
-        $command->setHelperSet(new HelperSet($helpers));
+        $command->setInstaller($installer);
 
         $input = $this->getMock('Symfony\Component\Console\Input\InputInterface');
         $output = $this->getMock('Symfony\Component\Console\Output\OutputInterface');
@@ -185,32 +77,5 @@ class GenerateVHostCommandTest extends \PHPUnit_Framework_TestCase
         $helpers['formatter'] = $formatterHelper;
 
         return $helpers;
-    }
-
-    private function expectProcess(\PHPUnit_Framework_MockObject_MockObject $processFactory, $command, $isSuccessful)
-    {
-        $process = $this->getMockBuilder('Symfony\Component\Process\Process')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $process->expects($this->atLeastOnce())
-            ->method('isSuccessful')
-            ->willReturn($isSuccessful);
-        $process->expects($this->once())
-            ->method('run');
-
-        $processFactory->expects($this->at($this->processesCounter++))
-            ->method('getProcess')
-            ->with($command)
-            ->willReturn($process);
-    }
-
-    private function mockContainerParameters(\PHPUnit_Framework_MockObject_MockObject $container, array $containerParams)
-    {
-        for ($i = 0; $i < count($containerParams); $i++) {
-            $container->expects($this->at($i))
-                ->method('getParameter')
-                ->with($containerParams[$i]['name'])
-                ->willReturn($containerParams[$i]['value']);
-        }
     }
 }
